6394d2ee4f8cd660f98345f0f865afc2b4e17b6c
It21165498 <99679538+It21165498@users.noreply.github.com>
2022-03-15 13:36:24 +0000

    Tute 6 done

--- Box.cpp
+++ Box.cpp
@@ -1,7 +1,41 @@
 #include "Box.h"
 
 // Implement setters and getters
+void Box::setLength(int l)
+{
+  length = l;
+}
 
-// Implemenet the calcVolume() unction
-int Box::calcVolume() {
+void Box::setWidth(int w)
+{
+  width = w;
+}
+
+void Box::setHeight(int h)
+{
+  height = h;
+}
+
+
+//getters
+int Box::getLength()
+{
+  return length;
+}
+
+int Box::getWidth()
+{
+  return width;
+}
+
+int Box::getHeight()
+{
+  return height;
+}
+ 
+
+// Implemenet the calcVolume() function
+int Box::calcVolume() 
+{
+  return length * width * height;
 }
--- Student.cpp
+++ Student.cpp
@@ -1,12 +1,17 @@
 #include "Student.h"
 #include <iostream>
+#include <string.h>
+using namespace std;
 
 // Assign studentId and name
-Student::assignDetails() {
-  
+void Student::assignDetails(int id, char sname[]) {
+  studentId = id;
+  strcpy(name,sname);
 }
 
 // Display StudentId and Name
-Student::display() {
+void Student::display() {
+  cout << "Student Id - " << studentId << endl;
+  cout << "Name - " << name << endl;
   
 }
